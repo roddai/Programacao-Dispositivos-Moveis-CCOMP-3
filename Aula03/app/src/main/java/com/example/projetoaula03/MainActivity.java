d59e69ee9f5d331e18c553b805f9211d977834ff
Rodrigo da Rosa <27532544869@fecap.br>
2025-08-27 21:06:25 -0300

    aplica código de funções para botões

--- Aula03/app/src/main/java/com/example/projetoaula03/MainActivity.java
+++ Aula03/app/src/main/java/com/example/projetoaula03/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.projetoaula03;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Livro livro1, livro2, livro3;
+
+        livro1 = new Livro("Desenvolvendo seu Primeiro Aplicativo Android","Luiz Carlos Querino Filho", 2017);
+        livro2 = new Livro("Livro 2","Autor 2", 2017);
+        livro3 = new Livro("Livro 3","Autor 3", 2017);
+
+        TextView textInfo = findViewById(R.id.textInfo);
+
+        Button button1 = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(v -> textInfo.setText(livro1.getInfo()));
+        button2.setOnClickListener(v -> textInfo.setText(livro2.getInfo()));
+        button3.setOnClickListener(v -> textInfo.setText(livro3.getInfo()));
     }
 }
